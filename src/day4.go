991c35c239e8688870769f8b7f86e6dff90b4b72
Angelo Bonicelli <angelo.bonicelli@gmail.com>
2023-12-05 22:44:52 +0100

    day4part2

--- src/day4.go
+++ src/day4.go
@@ -11,6 +11,12 @@ const (
 	two = 2
 )
 
+func intArrayToNumber(arr []int32) int {
+	kek, err := strconv.Atoi(string(arr))
+	check(err)
+	return kek
+}
+
 func day4PartOne(path string) int {
 	dat, err := os.ReadFile(path)
 	check(err)
@@ -20,19 +26,13 @@ func day4PartOne(path string) int {
 
 	for i, line := range lines {
 		line := line[strings.IndexByte(line, ':')+2:]
-		points += parseLine(line)
+		points += calculatePoints(line)
 		fmt.Printf("\nPoints after line %d - %d\n\n", i+1, points)
 	}
 	return points
 }
 
-func intArrayToNumber(arr []int32) int {
-	kek, err := strconv.Atoi(string(arr))
-	check(err)
-	return kek
-}
-
-func parseLine(line string) int {
+func calculatePoints(line string) int {
 	currNumber := make([]int32, 0)
 	var parsedNumber int
 	foundPipe := false
@@ -71,10 +71,6 @@ func parseLine(line string) int {
 		}
 	}
 
-	if len(currNumber) == 0 {
-		panic("Line didn't end with number")
-	}
-
 	parsedNumber = intArrayToNumber(currNumber)
 	if winningNumbers.Contains(parsedNumber) {
 		fmt.Printf("Match found! %d is in %s \n", parsedNumber, winningNumbers.ToString())
@@ -86,5 +82,80 @@ func parseLine(line string) int {
 }
 
 func day4PartTwo(path string) int {
-	return len(path)
+	dat, err := os.ReadFile(path)
+	check(err)
+
+	lines := strings.Split(string(dat), "\n")
+	var matches int
+
+	instances := make([]int, len(lines))
+	var instanceSize int
+	for i := range instances {
+		instances[i] = 1
+	}
+	wonCards := 0
+
+	for i, line := range lines {
+		line := line[strings.IndexByte(line, ':')+2:]
+
+		matches = findMatches(line)
+		instanceSize = instances[i]
+		wonCards += instanceSize
+		fmt.Printf(
+			"\nLine %d with %d instaces had %d matches\n",
+			i+1,
+			instanceSize,
+			matches,
+		)
+
+		for j := 1; j <= matches; j++ {
+			instances[i+j] += instanceSize
+		}
+	}
+
+	return wonCards
+}
+
+func findMatches(line string) int {
+	currNumber := make([]int32, 0)
+	var parsedNumber int
+	foundPipe := false
+
+	winningNumbers := NewSortedArray()
+	matches := 0
+
+	for _, char := range line {
+		if char == '|' {
+			foundPipe = true
+			continue
+		}
+
+		if char != ' ' {
+			currNumber = append(currNumber, char)
+			continue
+		}
+
+		if len(currNumber) != 0 {
+			parsedNumber = intArrayToNumber(currNumber)
+			currNumber = make([]int32, 0)
+
+			if !foundPipe {
+				winningNumbers.Insert(parsedNumber)
+				continue
+			}
+
+			if !winningNumbers.Contains(parsedNumber) {
+				continue
+			}
+
+			matches++
+		}
+	}
+
+	parsedNumber = intArrayToNumber(currNumber)
+	if winningNumbers.Contains(parsedNumber) {
+		matches++
+	}
+
+	return matches
 }
