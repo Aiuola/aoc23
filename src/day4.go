7d41129f1bc530bc0447d39bf7e02baf0f2dd677
Angelo Bonicelli <angelo.bonicelli@gmail.com>
2023-12-05 22:23:05 +0100

    day 4 part one

--- src/day4.go
+++ src/day4.go
@@ -1,7 +1,88 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	two = 2
+)
+
 func day4PartOne(path string) int {
-	return len(path)
+	dat, err := os.ReadFile(path)
+	check(err)
+
+	lines := strings.Split(string(dat), "\n")
+	points := 0
+
+	for i, line := range lines {
+		line := line[strings.IndexByte(line, ':')+2:]
+		points += parseLine(line)
+		fmt.Printf("\nPoints after line %d - %d\n\n", i+1, points)
+	}
+	return points
+}
+
+func intArrayToNumber(arr []int32) int {
+	kek, err := strconv.Atoi(string(arr))
+	check(err)
+	return kek
+}
+
+func parseLine(line string) int {
+	currNumber := make([]int32, 0)
+	var parsedNumber int
+	foundPipe := false
+
+	winningNumbers := NewSortedArray()
+
+	points, cardValue := 0, 1
+
+	for _, char := range line {
+		if char == '|' {
+			foundPipe = true
+			continue
+		}
+
+		if char != ' ' {
+			currNumber = append(currNumber, char)
+			continue
+		}
+
+		if len(currNumber) != 0 {
+			parsedNumber = intArrayToNumber(currNumber)
+			currNumber = make([]int32, 0)
+
+			if !foundPipe {
+				winningNumbers.Insert(parsedNumber)
+				continue
+			}
+
+			if !winningNumbers.Contains(parsedNumber) {
+				continue
+			}
+
+			fmt.Printf("Match found! %d is in %s \n", parsedNumber, winningNumbers.ToString())
+			points = cardValue
+			cardValue *= two
+		}
+	}
+
+	if len(currNumber) == 0 {
+		panic("Line didn't end with number")
+	}
+
+	parsedNumber = intArrayToNumber(currNumber)
+	if winningNumbers.Contains(parsedNumber) {
+		fmt.Printf("Match found! %d is in %s \n", parsedNumber, winningNumbers.ToString())
+		points = cardValue
+		cardValue *= two
+	}
+
+	return points
 }
 
 func day4PartTwo(path string) int {
--- src/day4Types.go
+++ src/day4Types.go
@@ -1,1 +1,63 @@
 package main
+
+type SortedArray struct {
+	elements []int
+}
+
+func NewSortedArray() *SortedArray {
+	return &SortedArray{elements: make([]int, 0)}
+}
+
+func (s *SortedArray) Insert(val int) {
+	size := len(s.elements)
+	if size == 0 {
+		s.elements = append(s.elements, val)
+		return
+	}
+	for i, elem := range s.elements {
+		if val > elem {
+			continue
+		}
+
+		s.insert(val, i)
+		return
+	}
+	s.elements = append(s.elements, val)
+	return
+}
+
+func (s *SortedArray) insert(val int, index int) {
+	s.elements = append(s.elements, 0)
+	copy(s.elements[index+1:], s.elements[index:])
+	s.elements[index] = val
+}
+
+func (s *SortedArray) Contains(val int) bool {
+	low, high := 0, len(s.elements)-1
+
+	if high == 0 {
+		return false
+	}
+	var mid int
+
+	for low <= high {
+		mid = (low + high) / 2
+		//fmt.Printf("num-%d %d %d %d\n", val, high, mid, low)
+		if val == s.elements[mid] {
+			return true
+		}
+
+		if val > s.elements[mid] {
+			low = mid + 1
+			continue
+		}
+
+		high = mid - 1
+	}
+
+	return false
+}
+
+func (s *SortedArray) ToString() any {
+	return stringRepresentation(s.elements)
+}
