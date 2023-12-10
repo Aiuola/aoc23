1ec09caaec362f8e7e05f17d060e3141be12ac0f
Angelo Bonicelli <angelo.bonicelli@gmail.com>
2023-12-10 16:49:14 +0100

    day8part1

--- src/day8.go
+++ src/day8.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -11,22 +12,97 @@ func day8PartOne(path string) int {
 	check(err)
 
 	lines := strings.Split(string(dat), "\n")
-	instructions := parseInstructions(lines[0])
+	// Right = true / left = false
+	directions := parseDirections(lines[0])
 	nodes := parseNodes(lines[2:])
+	sort.Sort(nodes)
 
-	for _, node := range nodes {
-		fmt.Printf("%s\n", node.val)
+	i := 0
+	cont := 0
+	for {
+		i = executeDirections(directions, nodes, i)
+		cont++
+		fmt.Printf("After the %d execution %s\n", cont, nodes[i])
+		if nodes[i].val == "ZZZ" {
+			break
+		}
 	}
 
-	return len(instructions)
+	return cont * len(directions)
 }
 
-func parseInstructions(line string) []bool {
-	return make([]bool, 0)
+func parseDirections(line string) Directions {
+	directions := make([]Direction, len(line))
+	for i, char := range line {
+		if char == 'L' {
+			continue
+		}
+		directions[i] = true
+	}
+	return directions
 }
 
-func parseNodes(lines []string) []*UnlinkedNode {
-	return make([]*UnlinkedNode, 0)
+func parseNodes(lines []string) UnlinkedNodes {
+	nodes := make([]*UnlinkedNode, len(lines))
+	for i, line := range lines {
+		nodes[i] = parseNode(line)
+	}
+	return nodes
+}
+
+func parseNode(line string) *UnlinkedNode {
+	var name, right, left string
+
+	parts := strings.Split(line, "=")
+
+	currWord := make([]int32, 0)
+	for _, char := range parts[0] {
+		if char == ' ' {
+			name = string(currWord)
+			currWord = make([]int32, 0)
+			break
+		}
+		currWord = append(currWord, char)
+	}
+
+	links := parts[1][2 : len(parts[1])-1]
+	for _, char := range links {
+		if char == ' ' {
+			continue
+		}
+		if char == ',' {
+			left = string(currWord)
+			currWord = make([]int32, 0)
+			continue
+		}
+		currWord = append(currWord, char)
+	}
+	right = string(currWord)
+
+	return NewUnlinkedNode(name, right, left)
+}
+
+func executeDirections(directions Directions, nodes UnlinkedNodes, startingIndex int) int {
+	currIndex := startingIndex
+	var newIndex int
+
+	for _, direction := range directions {
+		newIndex = nodes.Move(currIndex, direction)
+		if newIndex == len(nodes) {
+			panic(fmt.Sprintf(
+				"Couldn't move in direction %s from node %s",
+				direction,
+				nodes[currIndex],
+			))
+		}
+		currIndex = newIndex
+		/*fmt.Printf(
+			"Moved to node %s - at index %d\n",
+			nodes[currIndex],
+			currIndex,
+		)*/
+	}
+	return currIndex
 }
 
 func day8PartTwo(path string) int {
--- src/day8Types.go
+++ src/day8Types.go
@@ -1,23 +1,77 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 type UnlinkedNode struct {
 	val, right, left string
+}
+
+func (n UnlinkedNode) String() string {
+	return fmt.Sprintf("[%s] right %s left %s", n.val, n.right, n.left)
 }
 
 func NewUnlinkedNode(val string, right string, left string) *UnlinkedNode {
 	return &UnlinkedNode{val: val, right: right, left: left}
 }
 
-type Node struct {
-	val         string
-	right, left *Node
+func (n UnlinkedNode) GetNeighbour(direction Direction) string {
+	if direction {
+		return n.right
+	} else {
+		return n.left
+	}
 }
 
-func NewNode(val string) *Node {
-	return &Node{val: val, right: nil, left: nil}
+type UnlinkedNodes []*UnlinkedNode
+
+func (un UnlinkedNodes) Len() int {
+	return len(un)
 }
 
-func (n Node) link(r *Node, l *Node) {
-	n.right = r
-	n.left = l
+func (un UnlinkedNodes) Less(i, j int) bool {
+	return un[i].val < un[j].val
+}
+
+func (un UnlinkedNodes) Swap(i, j int) {
+	un[i], un[j] = un[j], un[i]
+}
+
+func (un UnlinkedNodes) String() string {
+	ret := "Nodes:"
+	for _, node := range un {
+		ret = fmt.Sprintf("%s\n%s", ret, node)
+	}
+	return ret
+}
+
+func (un UnlinkedNodes) Move(i int, direction Direction) int {
+	return un.Search(un[i].GetNeighbour(direction))
+}
+
+func (un UnlinkedNodes) Search(target string) int {
+	return sort.Search(len(un), func(i int) bool {
+		return un[i].val >= target
+	})
+}
+
+type Direction bool
+
+func (i Direction) String() string {
+	if i {
+		return "Right"
+	}
+	return "Left"
+}
+
+type Directions []Direction
+
+func (i Directions) String() string {
+	ret := "Directions:"
+	for _, direction := range i {
+		ret = fmt.Sprintf("%s\n%s", ret, direction)
+	}
+	return ret
 }
